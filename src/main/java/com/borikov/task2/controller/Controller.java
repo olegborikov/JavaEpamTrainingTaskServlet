82bccaef26e5424fffca937bd267d917cc687eb6
Oleg Borikov <olegborikov999@gmail.com>
2020-08-19 15:00:35 +0300

    add one more jsp(jsp-servlet-jsp)

--- src/main/java/com/borikov/task2/controller/Controller.java
+++ src/main/java/com/borikov/task2/controller/Controller.java
@@ -6,17 +6,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Calendar;
+import java.util.Date;
 
 @WebServlet(urlPatterns = "/controller")
 public class Controller extends HttpServlet {
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
     }
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
-        response.getWriter().println("Hello world");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        Date time = Calendar.getInstance().getTime();
+        request.setAttribute("time", time);
+        request.getRequestDispatcher("/jsp/result.jsp").forward(request,response);
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,11 +1,12 @@
 <%@ page contentType="text/html;charset=UTF-8" %>
 <html>
 <head>
-    <title>Title</title>
+    <title>Index</title>
 </head>
 <body>
+<h3>What time is this?</h3>
 <form action="controller" method="get">
-    <input type="submit" value="go"/>
+    <input type="submit" value="get time"/>
 </form>
 </body>
 </html>
